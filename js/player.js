63434c2e17c7670834f0f5ff706dfbfc9104826c
Reczeek <kreczek25@gmail.com>
2026-03-14 11:34:29 +0100

    obiekt gracza i statystyki

---
+++ js/player.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "Zniewolony",
+    hp: 100,
+    maxHp: 100,
+    attack: 10,
+    defense: 5,
+    gold: 50,
+    inventory: []
+}
--- js/scenes.js
+++ js/scenes.js
@@ -1,6 +1,11 @@
 const scenes = {
     explore: function() {
-        scene.innerHTML = "<h1>Eksploracja</h1>" + "<button id='btn-combat'>Walka</button>" + "<button id='btn-shop'>Sklep</button>" + "<button id='btn-explore'>Eksploracja</button>";
+        scene.innerHTML = 
+        "<h1>Eksploracja</h1>"
+        + "<div id='stats'>HP: " + player.hp + "/" + player.maxHp + " | Złoto: " + player.gold + "</div>"
+        + "<button id='btn-combat'>Walka</button>" 
+        + "<button id='btn-shop'>Sklep</button>" 
+        + "<button id='btn-explore'>Eksploracja</button>";
     },
     combat: function() {
         scene.innerHTML = "<h1>Walka</h1>" + "<button id='btn-explore'>Wróć</button>";
