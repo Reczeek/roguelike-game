e1d7e135046a2bc16734a6e7107349f6b53b2795
Reczeek <kreczek25@gmail.com>
2026-03-14 11:19:44 +0100

    przyciski scen

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ function loadScene(nazwa) {
     scenes[nazwa]();
 }
 
+function setupButtons() {
+    const btnCombat = document.getElementById('btn-combat');
+    const btnShop = document.getElementById('btn-shop');
+    const btnExplore = document.getElementById('btn-explore');
+    btnCombat.onclick = function() {
+        loadScene("combat");
+    }
+    btnShop.onclick = function() {
+        loadScene("shop");
+    }
+    btnExplore.onclick = function() {
+        loadScene("explore");
+    }
+}
 
-
-loadScene("combat");
+loadScene("explore");
+setupButtons();
--- js/scenes.js
+++ js/scenes.js
@@ -1,6 +1,6 @@
 const scenes = {
     explore: function() {
-        scene.innerHTML = "<h1>Eksploracja</h1>";
+        scene.innerHTML = "<h1>Eksploracja</h1>" + "<button id='btn-combat'>Walka</button>" + "<button id='btn-shop'>Sklep</button>" + "<button id='btn-explore'>Eksploracja</button>";
     },
     combat: function() {
         scene.innerHTML = "<h1>Walka</h1>";
